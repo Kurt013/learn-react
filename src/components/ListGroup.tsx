4dd217ef3aa1f59d453f34a8edd21588051d97ce
Almodovar, Kurt Vincent Almendral <121716283+Kurt013@users.noreply.github.com>
2026-01-30 01:37:25 +0800

    added state hook
    - learned hook concept (useState)
    - used it to dynamically add class 'active' on list item

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,16 +1,33 @@
+import { useState } from "react";
+
 function ListGroup() {
   const items = ["Kurt", "Vincent"];
 
-        
   const message = items.length === 0 && <p>No message found</p>;
+
+  let [selectedIndex, setSelectedIndex] = useState(-1);
 
   return (
     <>
       <h1>A list of goodies</h1>
       <ul className="list-group">
         {message}
-        {items.map((item) => {
-          return <li className="list-group-item">{item}</li>;
+        {items.map((item, index) => {
+          return (
+            <li
+              className={
+                selectedIndex === index
+                  ? "list-group-item active"
+                  : "list-group-item"
+              }
+              key={item}
+              onClick={() => {
+                setSelectedIndex(index);
+              }}
+            >
+              {item}
+            </li>
+          );
         })}
       </ul>
     </>
